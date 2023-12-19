45d647494a75979f9ce946e39033173500cf715e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2023-12-19 21:11:24 +0900

    Implement Huber loss and Median Absolute Deviation

--- src/lib.rs
+++ src/lib.rs
@@ -102,6 +102,29 @@ fn gauss_newton_update(param: &Param, src: &Vec<Measurement>, dst: &Vec<Measurem
     -update
 }
 
+fn rho(e: f64, k: f64) -> f64 {
+    debug_assert!(e >= 0.);
+    debug_assert!(k >= 0.);
+    let k_squared = k * k;
+    if e <= k_squared { e } else { 2. * k * e.sqrt() - k_squared }
+}
+
+fn drho(e: f64, k: f64) -> f64 {
+    debug_assert!(e >= 0.);
+    debug_assert!(k >= 0.);
+    let k_squared = k * k;
+    if e <= k_squared { 1. } else { k / e.sqrt() }
+}
+
+fn mad(input: &Vec<f64>) -> Option<f64> {
+    let m = match median::median(&input) {
+        None => return None,
+        Some(m) => m,
+    };
+    let a = input.iter().map(|e| { (e - m).abs() }).collect::<Vec<f64>>();
+    return median::median(&a);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -239,5 +262,57 @@ mod tests {
         let e0 = error(&initial_param, &src, &dst);
         let e1 = error(&updated_param, &src, &dst);
         assert!(e1 < e0 * 0.01);
+    }
+
+    #[test]
+    fn test_rho() {
+        assert_eq!(rho(0.1 * 0.1, 0.1), 0.1 * 0.1);
+        assert_eq!(rho(0.101 * 0.101, 0.1), 2. * 0.1 * 0.101 - 0.1 * 0.1);
+        assert_eq!(rho(0.09 * 0.09, 0.1), 0.09 * 0.09);
+    }
+
+    #[test]
+    fn test_drho() {
+        let e1 = (4.000_f64 + 0.001_f64).powi(2);
+        let e0 = 4.000_f64.powi(2);
+        let k = 4.0_f64;
+        let expected = (rho(e1, k) - rho(e0, k)) / (e1 - e0);
+        assert!((drho(e0, k) - expected).abs() < 1e-3);
+
+        let e1 = (0.10_f64 + 0.01_f64).powi(2);
+        let e0 = 0.10_f64.powi(2);
+        let k = 4.0_f64;
+        let expected = (rho(e1, k) - rho(e0, k)) / (e1 - e0);
+        assert_eq!(expected, drho(e0, k));
+
+        let e1 = (0.10_f64 + 0.0001_f64).powi(2);
+        let e0 = 0.10_f64.powi(2);
+        let k = 0.10_f64;
+        let expected = (rho(e1, k) - rho(e0, k)) / (e1 - e0);
+        assert!((drho(e0, k) - expected).abs() < 1e-3);
+
+        let e1 = (5.000_f64 + 0.001_f64).powi(2);
+        let e0 = 5.000_f64.powi(2);
+        let k = 4.0_f64;
+        let expected = (rho(e1, k) - rho(e0, k)) / (e1 - e0);
+        assert!((drho(e0, k) - expected).abs() < 1e-3);
+
+        let e1 = (10.000_f64 + 0.001_f64).powi(2);
+        let e0 = 10.000_f64.powi(2);
+        let k = 4.0_f64;
+        let expected = (rho(e1, k) - rho(e0, k)) / (e1 - e0);
+        assert!((drho(e0, k) - expected).abs() < 1e-3);
+    }
+
+    #[test]
+    fn test_mad() {
+        let a = vec![16., -16., -1., 8., -9., 4., -3., 17., 3., -7., 11., -1.];
+        assert_eq!(mad(&a), Some(7.5));
+
+        let a = vec![22., 1., -9., -35., -29., -40., -50., -45., 4.];
+        assert_eq!(mad(&a), Some(20.0));
+
+        let a = vec![-53., -36.];
+        assert_eq!(mad(&a), Some(8.5));
     }
 }
