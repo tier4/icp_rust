942fd37fac8cbdcc516f4a061ca62527ab8a0a1e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2023-12-21 20:03:45 +0900

    Avoid rustfmt for matrices

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-// use kdtree::distance::squared_equclidean;
+#![feature(stmt_expr_attributes)]
+
 use nalgebra::Cholesky;
 use nalgebra::{Matrix2, Matrix3, Vector2, Vector3};
 
@@ -16,9 +17,11 @@ fn calc_rt(param: &Vector3<f64>) -> (Matrix2<f64>, Vector2<f64>) {
     let cos = f64::cos(theta);
     let sin = f64::sin(theta);
 
+    #[rustfmt::skip]
     let R = Matrix2::new(
         cos, -sin,
-        sin, cos);
+        sin, cos,
+    );
 
     let vx = param[0];
     let vy = param[1];
@@ -37,10 +40,12 @@ fn calc_rt(param: &Vector3<f64>) -> (Matrix2<f64>, Vector2<f64>) {
 fn exp_se2(param: &Vector3<f64>) -> Matrix3<f64> {
     let (R, t) = calc_rt(param);
 
+    #[rustfmt::skip]
     Matrix3::new(
         R[(0, 0)], R[(0, 1)], t[0],
         R[(1, 0)], R[(1, 1)], t[1],
-        0., 0., 1.)
+        0., 0., 1.,
+    )
 }
 
 fn transform(param: &Param, landmark: &Measurement) -> Measurement {
@@ -70,14 +75,20 @@ fn inverse_3x3(matrix: &Matrix3<f64>) -> Option<Matrix3<f64>> {
     let m21 = matrix[(2, 1)];
     let m22 = matrix[(2, 2)];
 
-    let det = m00 * (m22*m11-m21*m12) - m10 * (m22*m01-m21*m02) + m20 * (m12*m01-m11*m02);
+    #[rustfmt::skip]
+    let det = m00 * (m22 * m11 - m21 * m12)
+            - m10 * (m22 * m01 - m21 * m02)
+            + m20 * (m12 * m01 - m11 * m02);
     if det == 0f64 {
         return None;
     }
+
+    #[rustfmt::skip]
     let mat = Matrix3::new(
-        m22*m11-m21*m12, -(m22*m01-m21*m02), m12*m01-m11*m02,
-        -(m22*m10-m20*m12), m22*m00-m20*m02, -(m12*m00-m10*m02),
-        m21*m10-m20*m11, -(m21*m00-m20*m01), m11*m00-m10*m01);
+        m22 * m11 - m21 * m12, -(m22 * m01 - m21 * m02), m12 * m01 - m11 * m02,
+        -(m22 * m10 - m20 * m12), m22 * m00 - m20 * m02, -(m12 * m00 - m10 * m02),
+        m21 * m10 - m20 * m11, -(m21 * m00 - m20 * m01), m11 * m00 - m10 * m01,
+    );
     Some(mat / det)
 }
 
@@ -209,10 +220,13 @@ mod tests {
         //        [ 0.        ,  0.        ,  1.        ]])
 
         let transform = exp_se2(&Vector3::new(-0.29638466, -0.15797957, -0.89885138));
+
+        #[rustfmt::skip]
         let expected = Matrix3::new(
-             0.6225093,  0.7826124, -0.32440305,
-            -0.7826124,  0.6225093, -0.01307704,
-             0.       ,  0.       ,  1.        );
+            0.6225093, 0.7826124, -0.32440305,
+            -0.7826124, 0.6225093, -0.01307704,
+            0., 0., 1.,
+        );
         assert!((transform - expected).norm() < 1e-6);
 
         // >>> a = np.array([-0.24295876,  0.95847196,  0.91052553])
@@ -222,10 +236,13 @@ mod tests {
         //        [ 0.        ,  0.        ,  1.        ]])
 
         let transform = exp_se2(&Vector3::new(-0.24295876, 0.95847196, 0.91052553));
+
+        #[rustfmt::skip]
         let expected = Matrix3::new(
             0.61333076, -0.78982617, -0.61778258,
-            0.78982617,  0.61333076,  0.72824049,
-            0.        ,  0.        ,  1.        );
+            0.78982617, 0.61333076, 0.72824049,
+            0., 0., 1.,
+        );
         assert!((transform - expected).norm() < 1e-6);
 
         // >>> a = np.array([10., -20., 0.])
@@ -235,10 +252,13 @@ mod tests {
         //        [  0.,   0.,   1.]])
 
         let transform = exp_se2(&Vector3::new(10., -20., 0.));
+
+        #[rustfmt::skip]
         let expected = Matrix3::new(
-             1., 0., 10.,
-             0., 1., -20.,
-             0., 0., 1.);
+            1., 0., 10.,
+            0., 1., -20.,
+            0., 0., 1.,
+        );
         assert!((transform - expected).norm() < 1e-6);
     }
 
@@ -253,14 +273,16 @@ mod tests {
     #[test]
     fn test_error() {
         let src = vec![
-           Measurement::new(-6., 9.),
-           Measurement::new(-1., 9.),
-           Measurement::new(-4., -4.)];
+            Measurement::new(-6., 9.),
+            Measurement::new(-1., 9.),
+            Measurement::new(-4., -4.),
+        ];
 
         let dst = vec![
-           Measurement::new(-4., 4.),
-           Measurement::new(0., 3.),
-           Measurement::new(-3., -8.)];
+            Measurement::new(-4., 4.),
+            Measurement::new(0., 3.),
+            Measurement::new(-3., -8.),
+        ];
 
         let param: Param = Vector3::new(10., 20., 0.01);
         let r0 = residual(&param, &src[0], &dst[0]);
@@ -274,10 +296,12 @@ mod tests {
     fn test_inverse_3x3() {
         let identity = Matrix3::identity();
 
+        #[rustfmt::skip]
         let matrix = Matrix3::new(
-            -3.64867356,  0.11236464, -7.60555263,
-            -3.56881707, -9.77855129,  0.50475873,
-            -9.34728378,  0.25373179, -7.55422161);
+            -3.64867356, 0.11236464, -7.60555263,
+            -3.56881707, -9.77855129, 0.50475873,
+            -9.34728378, 0.25373179, -7.55422161,
+        );
         let inverse = match inverse_3x3(&matrix) {
             Some(inverse) => inverse,
             None => panic!("Should return Some(inverse_matrix)"),
@@ -286,6 +310,7 @@ mod tests {
 
         assert!(inverse_3x3(&Matrix3::zeros()).is_none());
 
+        #[rustfmt::skip]
         let matrix = Matrix3::new(
             3.0, 1.0, 2.0,
             6.0, 2.0, 4.0,
@@ -293,10 +318,12 @@ mod tests {
         );
         assert!(inverse_3x3(&matrix).is_none());
 
+        #[rustfmt::skip]
         let matrix = Matrix3::new(
-            3.00792510e-38, -1.97985750e-45,  3.61627897e-44,
+            3.00792510e-38, -1.97985750e-45, 3.61627897e-44,
             7.09699991e-49, -3.08764937e-49, -8.31427092e-41,
-            2.03723891e-42, -3.84594910e-42,  1.00872600e-40);
+            2.03723891e-42, -3.84594910e-42, 1.00872600e-40,
+        );
         let inverse = match inverse_3x3(&matrix) {
             Some(inverse) => inverse,
             None => panic!("Should return Some(inverse_matrix)"),
