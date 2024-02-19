f5707c0d2f3de87145e8e69fb03110b6acaf6b62
IshitaTakeshi <ishitah.takeshi@gmail.com>
2024-02-19 17:47:20 +0900

    Remove 'param' member from 'Transform' struct

--- src/transform.rs
+++ src/transform.rs
@@ -5,17 +5,12 @@ use crate::types::Vector;
 pub struct Transform {
     pub rot: Rotation<2>,
     pub t: Translation<2>,
-    pub param: Vector<3>,
 }
 
 impl Transform {
     pub fn new(param: &Vector<3>) -> Self {
         let (rot, t) = se2::calc_rt(param);
-        Transform {
-            rot,
-            t,
-            param: *param,
-        }
+        Transform { rot, t }
     }
 
     pub fn transform(&self, landmark: &Vector<2>) -> Vector<2> {
