b4ca1e656758dc5920c1740185735b36bb46d336
IshitaTakeshi <ishitah.takeshi@gmail.com>
2024-02-21 19:18:58 +0900

    Format comments to fit each line widthin 100 characters

--- src/lib.rs
+++ src/lib.rs
@@ -150,50 +150,83 @@ pub fn icp_3dscan(
 ///
 /// Also, let $\mathrm{Exp}$ be an exponential map that maps a 3D vector to a 2D transform matrix.
 ///
-/// $$T = \mathrm{Exp}(\mathbf{x}) = \exp([\mathbf{x}]\_{\times}),\\ \mathbf{x} \in \mathbb{R}^{3}, T \in \mathrm{SE}(2)$$
+/// $$
+/// \begin{aligned}
+/// T
+/// &= \mathrm{Exp}(\mathbf{x}) \\\\
+/// &= \exp([\mathbf{x}]\_{\times}),\\; \mathbf{x} \in \mathbb{R}^{3}, T \in \mathrm{SE}(2)
+/// \end{aligned}
+/// $$
 ///
 /// where $\exp$ is the matrix exponential function, and we define
 /// $\mathrm{Log}$ as the inverse of $\mathrm{Exp}$, such that $\mathrm{Log}(T) = \mathbf{x}$.
 ///
-/// Utilizing the functions defined above, we express the parameter addition by the $\boxplus$ operator:
+/// Utilizing the functions defined above, we express the parameter addition
+/// by the $\boxplus$ operator:
 ///
 /// $$
 /// \mathbf{x} \boxplus \mathbf{y} = \mathrm{Log}(\mathrm{Exp}(\mathbf{x}) \mathrm{Exp}(\mathbf{y}))
 /// $$
 ///
-/// In the ICP's problem setting, given two associated points $\mathbf{a}, \mathbf{b} \in \mathbb{R}^{2}$,
-/// we aim to find the optimal tarnsformation parameter $\mathbf{x}^{*} \in \mathrm{R}^{2}$ that minimizes the difference between the transformed source point and
-/// the destination point.
+/// In the ICP's problem setting, given two associated points
+/// $\mathbf{a}, \mathbf{b} \in \mathbb{R}^{2}$, we aim to find the optimal tarnsformation
+/// parameter $\mathbf{x}^{*} \in \mathrm{R}^{2}$ that minimizes the difference between the
+/// transformed source point and the destination point.
 ///
 /// $$
-/// \mathbf{x}^{*} = {\arg\min}_{\mathbf{x}} || \mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x}) ||^{2},\\;\\;
-/// \mathbf{r}(\mathbf{a}, \mathbf{b};\mathbf{x}) = \mathrm{Exp}(\mathbf{x}) \dot{\mathbf{a}} - \dot{\mathbf{b}}
+/// \mathbf{x}^{*}
+/// = {\arg\min}_{\mathbf{x}} || \mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x}) ||^{2},\\;\\;
+/// \mathbf{r}(\mathbf{a}, \mathbf{b};\mathbf{x})
+/// = \mathrm{Exp}(\mathbf{x}) \dot{\mathbf{a}} - \dot{\mathbf{b}}
 /// $$
 ///
-/// The dot denotes the homogeneous operator, such that $\dot{\mathbf{a}} = \left[a_x, a_y, 1\right]$.
+/// The dot denotes the homogeneous operator,
+/// such that $\dot{\mathbf{a}} = \left[a_x, a_y, 1\right]$.
 ///
-/// To formulate the Gauss-Newton update, we calculate the jacobian of the residual function with respect to the pose parameter.
+/// To formulate the Gauss-Newton update, we calculate the jacobian of the residual function with
+/// respect to the pose parameter.
 ///
 /// \\[
 /// \begin{aligned}
 /// J
-/// &= \lim_{\delta \to \mathbf{0}} \frac{\mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x} \boxplus \mathbf{\delta}) - \mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x})}{\mathbf{\delta}}  \\\\
-/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}} \left\[\mathrm{Exp}(\mathbf{x} \boxplus \mathbf{\delta}) - \mathrm{Exp}(\mathbf{x}) \right\] \dot{\mathbf{a}}  \\\\
-/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}} \left\[\mathrm{Exp}(\mathbf{x})\mathrm{Exp}(\mathbf{\delta}) - \mathrm{Exp}(\mathbf{x}) \right\] \dot{\mathbf{a}}
+/// &= \lim_{\delta \to \mathbf{0}}
+/// \frac{
+/// \mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x} \boxplus \mathbf{\delta}) -
+/// \mathbf{r}(\mathbf{a}, \mathbf{b}; \mathbf{x})
+/// }{\mathbf{\delta}}  \\\\
+/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}}
+/// \left\[
+/// \mathrm{Exp}(\mathbf{x} \boxplus \mathbf{\delta}) -
+/// \mathrm{Exp}(\mathbf{x})
+/// \right\] \dot{\mathbf{a}}  \\\\
+/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}}
+/// \left\[
+/// \mathrm{Exp}(\mathbf{x})\mathrm{Exp}(\mathbf{\delta}) -
+/// \mathrm{Exp}(\mathbf{x})
+/// \right\] \dot{\mathbf{a}}
 /// \end{aligned}
 /// \\]
 ///
 /// Since $||\mathbf{\delta}||$ is small, we can approximate its exponential by
-/// the corresponding Lie algebra representation: $\mathrm{Exp}(\mathbf{\delta}) \approx I + [\mathbf{\delta}]_{\times}$.
+/// the corresponding Lie algebra representation:
+/// $\mathrm{Exp}(\mathbf{\delta}) \approx I + [\mathbf{\delta}]_{\times}$.
 ///
 /// Therefore,
 ///
 /// $$
 /// \begin{aligned}
 /// J
-/// &\approx \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}} \left[\mathrm{Exp}(\mathbf{x})(I + [\mathbf{\delta}]\_{\times}) - \mathrm{Exp}(\mathbf{x}) \right] \dot{\mathbf{a}}  \\\\
-/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}} \left[\mathrm{Exp}(\mathbf{x}) + \mathrm{Exp}(\mathbf{x}) {[\mathbf{\delta}]}\_{\times} - \mathrm{Exp}(\mathbf{x}) \right] \dot{\mathbf{a}} \\\\
-/// &= \lim_{\delta \to \mathbf{0}} \frac{1}{\mathbf{\delta}} \mathrm{Exp}(\mathbf{x}) {[\mathbf{\delta}]}\_{\times} \dot{\mathbf{a}}\\;.
+/// &\approx \lim_{\delta \to \mathbf{0}}
+/// \frac{1}{\mathbf{\delta}} \left[
+/// \mathrm{Exp}(\mathbf{x})(I + [\mathbf{\delta}]\_{\times}) -
+/// \mathrm{Exp}(\mathbf{x})
+/// \right] \dot{\mathbf{a}}  \\\\
+/// &= \lim_{\delta \to \mathbf{0}}
+/// \frac{1}{\mathbf{\delta}} \left[
+/// \mathrm{Exp}(\mathbf{x}) + \mathrm{Exp}(\mathbf{x}) {[\mathbf{\delta}]}\_{\times} - \mathrm{Exp}(\mathbf{x})
+/// \right] \dot{\mathbf{a}} \\\\
+/// &= \lim_{\delta \to \mathbf{0}}
+/// \frac{1}{\mathbf{\delta}} \mathrm{Exp}(\mathbf{x}) {[\mathbf{\delta}]}\_{\times} \dot{\mathbf{a}}\\;.
 /// \end{aligned}
 /// $$
 ///
